b8bfa0a96ececbab77c289e4638c4a72379ecae5
Anup <anupisri107@gmail.com>
2024-02-20 00:03:13 +0530

    inline css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 import { Parent } from './components/child_parent_comp.jsx/Parent';
 import Inlinecss from './udmey/Inlinecss';
-
+import name, {profession, degrees} from "./udmey/ImportExport";
 function App() {
   return (
   // <Parent/>
+  <>
+  
   <Inlinecss/>
+  <div className="">hello {name} {profession} </div>
+  </>
+
   );
 }
 
---
+++ src/udmey/ImportExport.jsx
@@ -0,0 +1,7 @@
+const name = "Anup Kumar";
+
+const profession = "Engineer"
+const degree = "B.Tech";
+
+export default name;
+export {profession , degree}
